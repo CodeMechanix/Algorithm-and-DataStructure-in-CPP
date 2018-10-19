ea029d567b1c8f5cfa26461a50db9ea7a4a719b8
Hasan Mahmud <hasan404cse@gmail.com>
2018-10-20 00:37:47 +0600

    Update Pattern Print.cpp

--- Algorithm Lab/Assignment - 1/Pattern Print.cpp
+++ Algorithm Lab/Assignment - 1/Pattern Print.cpp
@@ -1,17 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-void triangleRecursion(int x, int original)
+void triangleRecursion(int x, int n)
 {
-	if(x<=0) return;
-	triangleRecursion(x-1, original);
-	for(int i=0; i<(original-x); i++)
+	if(x==n-1) return;
+	int i,j,k;
+	triangleRecursion(x-1, n);
+	for( i=n; i>0; i--)
 	{
-		cout<<" ";
+		for(j=0; j<n-i; j++)
+		{
+			cout<<" ";
+		}
+		for( k=1; k<=i; k++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
 	}
-	for(int i=0; i<x; i++){
-		cout<<"* ";
-	}
-	cout<<endl;
 }
 void triangle(int x)
 {
